6a8d10186e5af263dc327f6aad7a5bc33ddc8645
JUB_Yoush <yoush1337@gmail.com>
2024-02-15 18:34:03 -0500

    new org snippets

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+ '(org-agenda-files
+   '("~/Dropbox/notes/planner/projects/godot.org" "/home/j/Dropbox/notes/planner/projects/misc.org" "/home/j/Dropbox/notes/planner/projects/jp.org" "/home/j/Dropbox/notes/planner/projects/blog.org" "/home/j/Dropbox/notes/planner/projects/dev.org" "/home/j/Dropbox/notes/planner/projects/work.org" "/home/j/Dropbox/notes/planner/projects/money.org" "/home/j/org/notes.org")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
